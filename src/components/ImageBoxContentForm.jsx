05fbf6446dd0030a3704ad007a6474d7f25f0a1d
oowtl <oowtl7425@gmail.com>
2022-04-04 22:43:21 +0900

    20220404 | write tag | css & image & validation

--- src/components/ImageBoxContentForm.jsx
+++ src/components/ImageBoxContentForm.jsx
@@ -6,7 +6,7 @@ const ImageBoxContentForm = (props) => {
   return (
       <form className={styles.ImageBoxContentForm}>
         <label htmlFor="emailValidate">Subscribe to our newsletter</label>
-        <div>
+        <div className={styles.ImageBoxContentFormBox}>
           <input placeholder="Enter your email" type="text" name="emailValidate"/>
           <i>
             <img src={`${process.env.PUBLIC_URL}/images/paper-plane.png`} alt="email-submit" />
